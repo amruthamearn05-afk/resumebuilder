708a1f4110fe39f0e831d96f17b0dc8f81b2fc56
Amrutha A <amruthamearn05@gmail.com>
2025-11-12 13:36:54 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,31 @@
+import { Route, Routes } from 'react-router-dom'
+import './App.css'
+import LandingPage from './pages/LandingPage'
+import ResumeGenerator from './pages/ResumeGenerator'
+import UserForm from './pages/UserForm'
+import History from './pages/History'
+import Pnf from './pages/Pnf'
+import Header from './components/Header'
+import Footer from './components/Footer'
+import ViewResume from './pages/ViewResume'
+
+function App() {
+  
+  return (
+    <>
+    <Header/>
+    <Routes>
+      <Route path='/' element={<LandingPage/>} />
+      <Route path='/resume' element={<ResumeGenerator/>} />
+      <Route path='/form' element={<UserForm/>} />
+      <Route path='/history' element={<History/>} />
+      {/* dynamic url are prefix with column, and dynamic value will be stored in variable after column ex - :id*/}
+      <Route path='/resume/:id/view' element={<ViewResume/>} />
+      <Route path='/*' element={<Pnf/>} />
+    </Routes>
+    <Footer/> 
+    </>
+  )
+}
+
+export default App
